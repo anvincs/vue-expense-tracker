7ffcc39510f7bcf0644093ee6ac13dd29160fc02
Anvin.C.Shaju <anvincsg@gmail.com>
2023-11-16 17:20:31 +0530

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,121 @@
+<template>
+  <Header />
+  <div class="container">
+    <Balance :total="+total" />
+    <IncomeExpenses
+      :income="+income"
+      :expenses="+expenses"
+    />
+    <TransactionList
+      :transactions="transactions"
+      @deleteTransaction="handleDeleteTransaction"
+    />
+    <!-- :transactions is a prop -->
+    <AddTransaction @addTransaction="handleAddTransaction" />
+    <!-- listening to the addTransaction custom event -->
+  </div>
+</template>
+
+<!-- <script>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+
+export default {
+  components: {
+    Header,
+    Balance,
+    IncomeExpenses,
+    TransactionList,
+    AddTransaction,
+  },
+};
+</script> -->
+
+<script setup>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+
+import { useToast } from "vue-toastification";
+
+import { ref, computed, onMounted } from "vue";
+// ref is used to make the values reactive
+// ref in Vue is the like useState in React
+
+const toast = useToast();
+
+const transactions = ref([]);
+
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem("transactions"));
+
+  if (savedTransactions) {
+    transactions.value = savedTransactions;
+  }
+});
+
+// Get total
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
+
+// Get Income
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
+// Get Expenses
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
+// Add Transaction
+const handleAddTransaction = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    ...transactionData,
+  });
+
+  saveTransactionsToLocalStorage();
+
+  toast.success("Transaction added");
+};
+
+// Delete Transaction
+const handleDeleteTransaction = (id) => {
+  transactions.value = transactions.value.filter(
+    (transaction) => transaction.id !== id
+  );
+
+  saveTransactionsToLocalStorage();
+
+  toast.success("Transaction deleted");
+};
+
+// Generate Unique ID
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 100000000);
+};
+
+// Save to localstorage
+const saveTransactionsToLocalStorage = () => {
+  localStorage.setItem("transactions", JSON.stringify(transactions.value));
+};
+</script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,87 @@
+<template>
+  <h3>History</h3>
+  <ul
+    id="list"
+    class="list"
+  >
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>Rs. {{ transaction.amount }}</span>
+      <button
+        @click="deleteTransaction(transaction.id)"
+        class="delete-btn"
+      >
+        x
+      </button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const emit = defineEmits(["deleteTransaction"]);
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
+
+const deleteTransaction = (id) => {
+  emit("deleteTransaction", id);
+  // emitting the deleteTransaction custom event
+};
+</script>
+
+<!-- "Options API"  -->
+<!-- <script>
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: "Flower", amount: -19.99 },
+        { id: 2, text: "Salary", amount: 299.97 },
+        { id: 3, text: "Book", amount: -10 },
+        { id: 4, text: "Camera", amount: 150 },
+      ],
+    };
+  },
+  methods: {},
+};
+</script> -->
+
+<!-- "Composition API"  -->
+<!-- <script>
+export default {
+  setup() {
+    const transactions = [
+      { id: 1, text: "Flower", amount: -19.99 },
+      { id: 2, text: "Salary", amount: 299.97 },
+      { id: 3, text: "Book", amount: -10 },
+      { id: 4, text: "Camera", amount: 150 },
+    ];
+    myFunction = () => {
+      console.log("myFunction");
+    };
+
+    return { transactions, myFunction };
+  },
+};
+</script> -->
+
+<!-- "Composition API" but better with the usage of 'setup' in the script tag -->
+<!-- <script setup>
+const transactions = [
+  { id: 1, text: "Flower", amount: -19.99 },
+  { id: 2, text: "Salary", amount: 299.97 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "Camera", amount: 150 },
+];
+</script> -->
+
+<!-- transactions moved to App.vue as it is used in multiple components (global state) -->
